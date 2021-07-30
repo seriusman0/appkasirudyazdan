625df97c54fc8f632e6b2d261cd54f239f3c6666
Seriusman Waruwu <seriusman0@gmail.com>
2021-07-31 01:43:57 +0700

    masuk

---
+++ db.sql
@@ -0,0 +1,189 @@
+-- MySQL dump 10.13  Distrib 8.0.26, for Linux (x86_64)
+--
+-- Host: localhost    Database: udyazdan
+-- ------------------------------------------------------
+-- Server version	8.0.26-0ubuntu0.20.04.2
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `tb_barang`
+--
+
+DROP TABLE IF EXISTS `tb_barang`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `tb_barang` (
+  `id_barang` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `nama_barang` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `harga_barang` int NOT NULL,
+  `stok_barang` int NOT NULL,
+  `modal_barang` int NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id_barang`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tb_barang`
+--
+
+LOCK TABLES `tb_barang` WRITE;
+/*!40000 ALTER TABLE `tb_barang` DISABLE KEYS */;
+INSERT INTO `tb_barang` VALUES ('089686060362','Pop Mie',6000,18,5000,'2021-07-10 00:31:35'),('089686600223','Cheetos',1500,4,1000,'2021-07-21 05:27:35'),('8886015203115','Good Time',5500,10,5000,'2021-07-10 00:30:49'),('8994075230870','Momogi',500,100,450,'2021-07-10 00:29:03'),('8998009010620','Ultra Milk Coklat',11000,2,10500,'2021-07-21 05:26:05');
+/*!40000 ALTER TABLE `tb_barang` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tb_cart`
+--
+
+DROP TABLE IF EXISTS `tb_cart`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `tb_cart` (
+  `id_cart` int NOT NULL AUTO_INCREMENT,
+  `id_barang` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `jumlah` int NOT NULL,
+  `update_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id_cart`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tb_cart`
+--
+
+LOCK TABLES `tb_cart` WRITE;
+/*!40000 ALTER TABLE `tb_cart` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tb_cart` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tb_history`
+--
+
+DROP TABLE IF EXISTS `tb_history`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `tb_history` (
+  `id_history` int NOT NULL AUTO_INCREMENT,
+  `id_barang` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `jumlah` int NOT NULL,
+  `id_payment` int NOT NULL,
+  `update_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id_history`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tb_history`
+--
+
+LOCK TABLES `tb_history` WRITE;
+/*!40000 ALTER TABLE `tb_history` DISABLE KEYS */;
+INSERT INTO `tb_history` VALUES (1,'089686060362',1,10,'2021-07-10 00:38:40'),(2,'089686060362',1,11,'2021-07-10 04:52:53');
+/*!40000 ALTER TABLE `tb_history` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tb_payment`
+--
+
+DROP TABLE IF EXISTS `tb_payment`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `tb_payment` (
+  `id_payment` int NOT NULL AUTO_INCREMENT,
+  `penjual` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `pembeli` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `total_bayar` int NOT NULL,
+  `update_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id_payment`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tb_payment`
+--
+
+LOCK TABLES `tb_payment` WRITE;
+/*!40000 ALTER TABLE `tb_payment` DISABLE KEYS */;
+INSERT INTO `tb_payment` VALUES (1,'Serius','Aldo',7000,'2021-07-09 13:05:08'),(2,'Serius','Budi',10000,'2021-07-09 13:06:46'),(3,'Serius','Martinus',30000,'2021-07-09 13:21:22'),(4,'Serius','Nomar',34000,'2021-07-09 13:36:22'),(5,'Intan','Putra',25000,'2021-07-09 22:24:08'),(6,'COba','Coba',5000,'2021-07-09 23:36:39'),(7,'Serius','Tamu',10000,'2021-07-10 00:32:22'),(8,'Serius','Tamu',6000,'2021-07-10 00:33:28'),(9,'Serius','Waruwu',6000,'2021-07-10 00:36:37'),(10,'Serius','Tamu',6000,'2021-07-10 00:38:40'),(11,'Serius','Waruwu',10000,'2021-07-10 04:52:53');
+/*!40000 ALTER TABLE `tb_payment` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tb_transaksi`
+--
+
+DROP TABLE IF EXISTS `tb_transaksi`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `tb_transaksi` (
+  `id_transaksi` int NOT NULL AUTO_INCREMENT,
+  `id_user` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `pembeli` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `total_tagihan` int NOT NULL,
+  `total_bayar` int NOT NULL,
+  `deskripsi` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci,
+  `at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id_transaksi`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tb_transaksi`
+--
+
+LOCK TABLES `tb_transaksi` WRITE;
+/*!40000 ALTER TABLE `tb_transaksi` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tb_transaksi` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tb_user`
+--
+
+DROP TABLE IF EXISTS `tb_user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `tb_user` (
+  `id_user` int NOT NULL AUTO_INCREMENT,
+  `nama_user` varchar(30) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `nohp_user` varchar(14) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `username` varchar(40) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`id_user`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tb_user`
+--
+
+LOCK TABLES `tb_user` WRITE;
+/*!40000 ALTER TABLE `tb_user` DISABLE KEYS */;
+INSERT INTO `tb_user` VALUES (1,'Seriusman Waruwu','082274839576','serius','$2y$12$gJAKxzlmYOFGUXfKK.YCae5ei3pD1xKs87esm8cfqodMqFni9Ktjq'),(9,'Yuniman Waruwu','082246363510','niman','$2y$10$1OJbNydtyqLDoZfgrhzC1O.Fiskgakk0aZfifZr6Bdxrp9t8MayQ.');
+/*!40000 ALTER TABLE `tb_user` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-07-31  1:43:49
