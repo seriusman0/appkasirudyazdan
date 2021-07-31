f70e40a80ff55beaa52cb25ae23a0d5313617e0d
serius <seriusman0@gmail.com>
2021-08-01 02:02:13 +0700

    ubah var config

--- conf/config.php
+++ conf/config.php
@@ -4,4 +4,5 @@ $username = "root";
 $password = 'tmDevFlats0987^(';
 $database = "udyazdan";
 
-$conn = mysqli_connect($server, $username, $password, $database) or die("Gagal");
+// $conn = mysqli_connect($server, $username, $password, $database) or die("Gagal");
+$conn = new mysqli($server, $username, $password, $database,8000) or die("Gagal");
