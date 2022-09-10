56ab214956acb7936a262ce011f8a7706f9869b3
serius <seriusman0@gmail.com>
2022-09-10 16:04:43 +0700

    database online

---
+++ 3.sql
@@ -0,0 +1,238 @@
+-- MySQL dump 10.13  Distrib 8.0.30, for Linux (x86_64)
+--
+-- Host: localhost    Database: kantinkejujuran
+-- ------------------------------------------------------
+-- Server version	8.0.30-0ubuntu0.22.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `pelanggan`
+--
+
+DROP TABLE IF EXISTS `pelanggan`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `pelanggan` (
+  `id_pelanggan` int NOT NULL AUTO_INCREMENT,
+  `nama_pelanggan` varchar(50) NOT NULL,
+  PRIMARY KEY (`id_pelanggan`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `pelanggan`
+--
+
+LOCK TABLES `pelanggan` WRITE;
+/*!40000 ALTER TABLE `pelanggan` DISABLE KEYS */;
+/*!40000 ALTER TABLE `pelanggan` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `pembayaran_hutang`
+--
+
+DROP TABLE IF EXISTS `pembayaran_hutang`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `pembayaran_hutang` (
+  `id_pembayaran_hutang` int NOT NULL AUTO_INCREMENT,
+  `id_hutang` int DEFAULT NULL,
+  `jumlah` int DEFAULT NULL,
+  `oleh` varchar(10) DEFAULT NULL,
+  `tgl` date DEFAULT (curdate()),
+  PRIMARY KEY (`id_pembayaran_hutang`)
+) ENGINE=InnoDB AUTO_INCREMENT=36 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `pembayaran_hutang`
+--
+
+LOCK TABLES `pembayaran_hutang` WRITE;
+/*!40000 ALTER TABLE `pembayaran_hutang` DISABLE KEYS */;
+/*!40000 ALTER TABLE `pembayaran_hutang` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tb_barang`
+--
+
+DROP TABLE IF EXISTS `tb_barang`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `tb_barang` (
+  `id_barang` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `nama_barang` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `harga_barang` int NOT NULL,
+  `stok_barang` int DEFAULT '1',
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id_barang`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tb_barang`
+--
+
+LOCK TABLES `tb_barang` WRITE;
+/*!40000 ALTER TABLE `tb_barang` DISABLE KEYS */;
+INSERT INTO `tb_barang` VALUES ('1','2',3,1,'2022-09-10 03:06:36');
+/*!40000 ALTER TABLE `tb_barang` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tb_cart`
+--
+
+DROP TABLE IF EXISTS `tb_cart`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `tb_cart` (
+  `id_cart` int NOT NULL AUTO_INCREMENT,
+  `id_barang` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `jumlah` int DEFAULT '1',
+  `update_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id_cart`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tb_cart`
+--
+
+LOCK TABLES `tb_cart` WRITE;
+/*!40000 ALTER TABLE `tb_cart` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tb_cart` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tb_history`
+--
+
+DROP TABLE IF EXISTS `tb_history`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `tb_history` (
+  `id_history` int NOT NULL AUTO_INCREMENT,
+  `id_barang` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `jumlah` int NOT NULL,
+  `id_payment` int NOT NULL,
+  `update_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id_history`)
+) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tb_history`
+--
+
+LOCK TABLES `tb_history` WRITE;
+/*!40000 ALTER TABLE `tb_history` DISABLE KEYS */;
+INSERT INTO `tb_history` VALUES (8,'8991389220016',2,14,'2022-08-29 08:09:44'),(9,'8991389220016',10,15,'2022-08-29 09:18:16'),(10,'8991389220016',1,16,'2022-08-29 09:19:00'),(11,'8991389220009',2,17,'2022-08-29 09:19:33'),(12,'8991389220009',1,18,'2022-08-29 09:44:03'),(13,'8991389220009',1,19,'2022-08-29 09:48:02'),(14,'8991389220009',1,19,'2022-08-29 09:48:02'),(15,'8991389220009',2,20,'2022-08-29 09:49:33'),(16,'8991389220009',2,21,'2022-08-29 09:50:30'),(17,'8991389220009',2,22,'2022-08-29 09:51:50'),(18,'8991389220016',5,23,'2022-08-29 10:13:39'),(19,'8991389220009',1,23,'2022-08-29 10:13:39'),(20,'6971826050293',2,24,'2022-09-09 01:53:38');
+/*!40000 ALTER TABLE `tb_history` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tb_payment`
+--
+
+DROP TABLE IF EXISTS `tb_payment`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `tb_payment` (
+  `id_payment` int NOT NULL AUTO_INCREMENT,
+  `penjual` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `pembeli` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `total_bayar` int NOT NULL,
+  `update_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id_payment`)
+) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tb_payment`
+--
+
+LOCK TABLES `tb_payment` WRITE;
+/*!40000 ALTER TABLE `tb_payment` DISABLE KEYS */;
+INSERT INTO `tb_payment` VALUES (14,'','Serius',8000,'2022-08-29 08:09:44'),(15,'','serius',40000,'2022-08-29 09:18:15'),(16,'','Termos',5000,'2022-08-29 09:19:00'),(17,'','masa bodo',40000,'2022-08-29 09:19:33'),(18,'','SErisuiuer',20000,'2022-08-29 09:44:03'),(19,'','Jhon',4000,'2022-08-29 09:48:02'),(20,'','Jhon Wick',4000,'2022-08-29 09:49:33'),(21,'','Jhon Wick 1',4000,'2022-08-29 09:50:30'),(22,'','Jhon Wick 2',4000,'2022-08-29 09:51:50'),(23,'','Awang',22000,'2022-08-29 10:13:38'),(24,'','ssss',2000,'2022-09-09 01:53:38');
+/*!40000 ALTER TABLE `tb_payment` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tb_transaksi`
+--
+
+DROP TABLE IF EXISTS `tb_transaksi`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `tb_transaksi` (
+  `id_transaksi` int NOT NULL AUTO_INCREMENT,
+  `id_user` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `pembeli` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `total_tagihan` int NOT NULL,
+  `total_bayar` int NOT NULL,
+  `deskripsi` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci,
+  `at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id_transaksi`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tb_transaksi`
+--
+
+LOCK TABLES `tb_transaksi` WRITE;
+/*!40000 ALTER TABLE `tb_transaksi` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tb_transaksi` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tb_user`
+--
+
+DROP TABLE IF EXISTS `tb_user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `tb_user` (
+  `id_user` int NOT NULL AUTO_INCREMENT,
+  `nama_user` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `nohp_user` varchar(14) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `username` varchar(40) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `otoritas` text COLLATE utf8mb4_unicode_ci,
+  PRIMARY KEY (`id_user`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tb_user`
+--
+
+LOCK TABLES `tb_user` WRITE;
+/*!40000 ALTER TABLE `tb_user` DISABLE KEYS */;
+INSERT INTO `tb_user` VALUES (9,'Seriusman Waruwu','082274839576','serius','$2y$10$pWi1W2fCX3doeoxbf8JAn.D6HAPKkjCWx9PC.fGnAAyXZ.TatZVRq',NULL),(10,'Nomar Samaa','0','nomar','nomars',NULL);
+/*!40000 ALTER TABLE `tb_user` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-09-10 10:53:11
--- api/add_barang.php
+++ api/add_barang.php
@@ -2,13 +2,15 @@
 
 include "../conf/config.php";
 
-$id_barang = json_decode($_POST['id_barang']);
-$nama_barang = json_decode($_POST['nama_barang']);
-$harga_barang = json_decode($_POST['harga_barang']);
+
+$id_barang = $_POST['id_barang'];
+$nama_barang = $_POST['nama_barang'];
+$harga_barang = str_replace(['+', '-'], '', filter_var($_POST['harga_barang'], FILTER_SANITIZE_NUMBER_INT));
+
 
 $check = mysqli_num_rows(mysqli_query($conn, "SELECT * FROM tb_barang where id_barang = '$id_barang'"));
 
-$query = mysqli_query($conn, "select * from tb_barang  where id_barang = '$id_barang'");
+$query = mysqli_query($conn, "select * from tb_barang order by created_at DESC");
 
 if ($check > 0) {
     echo json_encode([
@@ -16,9 +18,23 @@ if ($check > 0) {
         "message" => "Barang sudah ada",
         "body" => mysqli_fetch_all($query, MYSQLI_ASSOC)
     ]);
-} else
-    echo json_encode([
-        "status" => "success",
-        "message" => "Barang belum ada di DB",
-        "body" => mysqli_fetch_all($query, MYSQLI_ASSOC)
-    ]);
+} else {
+    $result = mysqli_query($conn, "INSERT INTO tb_barang SET 
+        id_barang = '$id_barang',  
+        nama_barang = '$nama_barang',  
+        harga_barang = '$harga_barang'
+    ");
+
+    if (mysqli_affected_rows($conn) > 0) {
+        echo json_encode([
+            "status" => "success",
+            "message" => "Affected Rows " . mysqli_affected_rows($conn),
+            "body" => mysqli_fetch_all($query, MYSQLI_ASSOC)
+        ]);
+    } else
+        echo json_encode([
+            "status" => "gagal",
+            "message" => "Affected Rows " . mysqli_affected_rows($conn),
+            "body" => mysqli_fetch_all($query, MYSQLI_ASSOC)
+        ]);
+}
