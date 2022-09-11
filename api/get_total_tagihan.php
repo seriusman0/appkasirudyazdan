af3b381187e7a15e3a4c7e9d9486c6868ba5bdbf
serius <seriusman0@gmail.com>
2022-09-11 10:32:49 +0700

    memisahkan total pembayaran dengan tagihan

--- api/get_total_tagihan.php
+++ api/get_total_tagihan.php
@@ -1,12 +1,10 @@
+
 <?php
 
 include "../conf/config.php";
 
-$tb_payment       = mysqli_query($conn, "SELECT * from tb_payment");
-$tb_history       = mysqli_query($conn, "SELECT * from tb_history");
-$tb_barang       = mysqli_query($conn, "SELECT * from tb_barang");
-
-$data = mysqli_fetch_all($tb_payment, MYSQLI_ASSOC);
+// $id_payment = $_POST['id_payment'];
+$id_payment = 27;
 
 function totalTagihan($id_payment)
 {
@@ -14,6 +12,7 @@ function totalTagihan($id_payment)
     $result = mysqli_fetch_array(mysqli_query($conn, "select sum(tb_barang.harga_barang) from tb_barang, tb_history where tb_history.id_barang = tb_barang.id_barang and tb_history.id_payment='$id_payment'"));
     return $result[0];
 }
-$result = array();
 
-echo json_encode($data);
+
+
+echo json_encode(["total_tagihan" => totalTagihan($id_payment)]);
